bfca34e557edc541a27c5948ef14a36ab899ce3a
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-09-17 01:07:22 +0530

    init

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+import { Flower } from "lucide-react";
+
+export default function Navbar(){
+  return <div>
+    <div className="flex items-center gap-1 font-semibold text-md">
+      <Flower />
+      DocNext
+    </div>
+  </div>
+}
