ad31f99eb8a4bd033182d947463b7957c9f07c44
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-09-28 05:06:51 +0530

    feat: clean command to cleanup the template

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 
 import chalk from "chalk";
-import {Command} from "commander"
+import { Command } from "commander"
 import { createSpinner } from "nanospinner";
-import {exec} from "node:child_process"
+import { exec } from "node:child_process"
 import figlet from "figlet"
 import gradient from "gradient-string"
+import { confirm } from '@inquirer/prompts';
+import { readdirSync,  rmdir, rmdirSync, rmSync } from "node:fs";
+import path from "node:path";
+
 const program = new Command();
 
-async function welcome():Promise<void>{
-  await new Promise<void>(resolve => figlet("DocNext", (err,msg)=>{
+async function welcome(): Promise<void> {
+  await new Promise<void>(resolve => figlet("DocNext", (err, msg) => {
     console.log(gradient.morning(msg))
     resolve()
   }))
@@ -20,45 +24,79 @@ async function welcome():Promise<void>{
 program
   .name('docnext')
   .description('CLI to make documentation easier!')
-  .version('1.0.0')
+  .version('1.0.1')
+  .option('-y, --yes', 'bypass checks')
 
 program.parse(process.argv);
 
 program.command('create [projectName]')
-  .description('Create a DocNext project') 
-  // .option('-n, --name <string>', 'name of the project', 'hello')
+  .description('create a DocNext project')
   .action(async (projectName) => {
     await welcome()
     const command = `npx create-next-app -e https://github.com/kartikm7/docnext/tree/master/template ${projectName ?? '.'}`
     const spinner = createSpinner(' Cloning the repository...').start()
-        
+
     // here we are cloning the repository
     await new Promise<void>((resolve, reject) => {
       exec(command, (err, stdout, stderr) => {
         // incase it fails, we are exiting the cli
-        if(err){
-          spinner.error({text:String(err)})
+        if (err) {
+          spinner.error({ text: String(err) })
           reject(err)
           process.exit(1)
         }
         // incase it does not fail we move onto the next step
         resolve()
       })
-    })    
-    const npmi = projectName ? `cd ${projectName} && npm i` : `npm i`  
-    spinner.update({text: chalk.yellow(' Installing dependencies...')}).start()
+    })
+    // installing the dependencies
+    const npmi = projectName ? `cd ${projectName} && npm i` : `npm i`
+    spinner.update({ text: chalk.yellow(' Installing dependencies...') }).start()
     await new Promise<void>((resolve, reject) => {
       exec(npmi, (err, stdout, stderr) => {
-        if(err){
-          spinner.error({text:String(err)})
+        if (err) {
+          spinner.error({ text: String(err) })
           reject(err)
           process.exit(1)
         }
-        spinner.success({text: chalk.green(' Voila done!')})
+        spinner.success({ text: chalk.green(' Voila done!') })
         resolve()
       })
     })
     process.exit(0)
   });
+
+program.command('clean')
+  .description('cleans up the template')
+  .action(async () => {
+    await welcome()
+    const yes = program.opts().yes;
+    // incase there is no flag
+    console.log('This deletes the template files, giving you a blank slate')
+    console.log(chalk.bgYellow('Please make sure you are in the root of the project'))
+    if (!yes) {
+      const prompt1 = await confirm({ message: 'Are you sure?' })
+      if (!prompt1) {
+        console.log("You can use this command "+ gradient.morning('when you are ready!'))
+        process.exit(0)
+      }
+    }
+    // current directory
+    const currentDir = process.cwd()
+    const spinner = createSpinner(' Cleaning the Files...').start()
+    const docsDirectory = path.join(currentDir, 'src', 'app', 'docs')
+    const contents = readdirSync(docsDirectory)
+    // deleting the folder contents
+    for(let idx = 0; idx < contents.length; idx++ ){
+      const content = contents[idx]
+      // layout and page are also located at the root
+      if(content != 'boilerplate' && content != 'layout.tsx' && content != 'page.mdx'){ 
+        rmSync(path.join(docsDirectory,content),{recursive:true,force: true})
+      }
+    }
+
+    spinner.success({text: ' Have fun '+gradient.morning('documenting!')})
+    process.exit(0)
+  })
 
 program.parse();
